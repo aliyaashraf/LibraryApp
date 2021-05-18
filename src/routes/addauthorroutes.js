bd6ba6767e1a135bd01922024397c0160ad0ca71
aliyaashref <aliyaashref@gmail.com>
2021-05-18 22:44:50 +0530

    added multar

--- src/routes/addauthorroutes.js
+++ src/routes/addauthorroutes.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const addauthorRouter = express.Router();
-const Authordata = require('../modal/Authordata');
+const Authordata = require("../modal/Authordata");
 
-const multer  = require('multer');
+const multer = require("multer");
 const storage = multer.diskStorage({
-    destination: function(request, file, callback){
-        callback(null, './public/images');
-    },
-    filename: function(request, file, callback){
-        callback(null,Date.now() + file.originalname);
-    },
+  destination: function (request, file, callback) {
+    callback(null, "./public/images");
+  },
+  filename: function (request, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
 });
 
 const upload = multer({
-    storage: storage
+  storage: storage,
 });
 
-
-
-
-
-
 function router(nav) {
-
   addauthorRouter.get("/", function (req, res) {
     res.render("addauthor", {
       nav,
@@ -30,18 +24,16 @@ function router(nav) {
     });
   });
   //for accessign from url we use colon :
-  addauthorRouter.post("/add", upload.single('image'),function (req, res) {
+  addauthorRouter.post("/add", upload.single("image"), function (req, res) {
     var item = {
       aname: req.body.aname,
       books: req.body.books,
       desc: req.body.desc,
       image: req.file.filename,
-      
-    }
+    };
     var author = Authordata(item);
-    author.save();//saving to db
-    res.redirect('/authors');
-
+    author.save(); //saving to db
+    res.redirect("/authors");
   });
   return addauthorRouter;
 }
--- src/routes/addbookroutes.js
+++ src/routes/addbookroutes.js
@@ -1,29 +1,39 @@
 const express = require("express");
 const addbookRouter = express.Router();
-const Bookdata = require('../modal/Bookdata');
+const Bookdata = require("../modal/Bookdata");
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+    callback(null, "./public/images");
+  },
+  filename: function (request, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 function router(nav) {
-  
   addbookRouter.get("/", function (req, res) {
     res.render("addbook", {
       nav,
       title: "Library",
-    
     });
   });
   //for accessign from url we use colon :
-  addbookRouter.post("/add", function (req, res) {
+  addbookRouter.post("/add", upload.single("image"), function (req, res) {
     var item = {
       title: req.body.title,
       author: req.body.author,
       genre: req.body.genre,
       description: req.body.description,
-      image: req.body.image
-      
-    }
+      image: req.file.filename,
+    };
     var book = Bookdata(item);
-    book.save();//saving to db
-    res.redirect('/books');
-
+    book.save(); //saving to db
+    res.redirect("/books");
   });
   return addbookRouter;
 }
